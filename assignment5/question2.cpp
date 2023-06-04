66a3286c57dc41d6fcd6f5ce4dc3012eaf138675
Riteshkgiitbbs <123089654+Riteshkgiitbbs@users.noreply.github.com>
2023-06-04 20:12:15 +0530

    Update question2.cpp

--- assignment5/question2.cpp
+++ assignment5/question2.cpp
@@ -1,1 +1,28 @@
+You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase **may be** incomplete.
+Given the integer n, return *the number of **complete rows** of the staircase you will build*.
 
+ /*---------my code is start from here-------------*/
+#include <iostream>
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int i = 1;
+        int count = 0;
+        while (n >= 0) {
+            n -= i;
+            count++;
+            i++;
+        }
+        return count - 1;
+    }
+};
+
+int main() {
+    Solution solution;
+    int n;
+    std::cout << "Enter the number of coins: ";
+    std::cin >> n;
+    int result = solution.arrangeCoins(n);
+    std::cout << "Number of complete rows: " << result << std::endl;
+    return 0;
+}
