38c30f489fc6ff606ec1cf080a8666ca7e99e4f7
Riteshkgiitbbs <123089654+Riteshkgiitbbs@users.noreply.github.com>
2023-06-23 12:47:55 +0530

    Create find kth smallestelement.cpp

---
+++ binarysearchtree/find kth smallestelement.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h> 
+/*************************************************************
+ 
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode 
+    {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+int solve(BinaryTreeNode<int>* root, int& i, int k) {
+    if (root == NULL) {
+        return -1;
+    }
+    
+    int left = solve(root->left, i, k);
+    if (left != -1) {
+        return left;
+    }
+    
+    i++;
+    if (i == k) {
+        return root->data;
+    }
+    
+    int right=solve(root->right, i, k);
+    return right;
+}
+
+int kthSmallest(BinaryTreeNode<int>* root, int k) {
+    int i = 0;
+    return solve(root, i, k);
+}
